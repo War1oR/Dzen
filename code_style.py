b121e25b558ff2d27a67e4502b3633381645b6f6
War1oR <warhome@yandex.ru>
2013-11-18 09:43:32 +0400

    + Stroki

---
+++ code_style.py
@@ -0,0 +1,16 @@
+#Длинные строки кода и их продолжения
+
+
+def ras(first, second, third,
+        fourth, fifth, sixth):
+    output = (first + second + third
+              + fourth + fifth + sixth)
+    return output
+
+VeryLong = 'Очень длинная строка' + \
+    'которая продолжается'
+#неявное объеденение
+print('o''n''e')
+text = ('Первая и'' вторая')
+print(text)
+
