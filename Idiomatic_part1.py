29c4dd82e874530fe1588e23995cf95f8430ba32
War1oR <warhome@yandex.ru>
2013-11-18 11:06:48 +0400

    + in

--- Idiomatic_part1.py
+++ Idiomatic_part1.py
@@ -1,1 +1,14 @@
+#Good
+d = {'a': '1', 'b': '2', 'c': '3'}
+for key in d:
+    print(key, end=' ')
+print()
+#Bad
+for key in d.keys():
+    print(key, end=' ')
+print()
 
+#Good
+key = 'a'
+if key in d:
+    print(d[key])
