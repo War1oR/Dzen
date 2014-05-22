3240d4bc9279f3d5122652ceeadd92160c482f81
War1oR <warhome@yandex.ru>
2014-05-22 16:27:38 +0400

    ~ update myUnittest

--- python/mystdlib/myUnittest/file4test.py
+++ python/mystdlib/myUnittest/file4test.py
@@ -1,12 +1,13 @@
 __author__ = 'warior'
-__mastermind__ = ['https://docs.python.org/3/library/unittest.html']
-
+__mastermind__ = 'https://docs.python.org/3/library/unittest.html'
+__version__ = 0.2
 
 class Widget():
 
     def __init__(self, arg):
         self.name = arg
         self.size = (510, 50)
+        self.q = ('foo', )
 
     def resize(self, x, y):
         self.size = (x, y)
--- python/mystdlib/myUnittest/test.py
+++ python/mystdlib/myUnittest/test.py
@@ -1,20 +1,52 @@
 __author__ = 'warior'
-__mastermind__ = ['https://docs.python.org/3/library/unittest.html']
+__mastermind__ = 'https://docs.python.org/3/library/unittest.html'
 
 import unittest
 import file4test
 
 
 class WidgetTestCase(unittest.TestCase):
+    #Срабатывает до тестов
     def setUp(self):
         self.widget = file4test.Widget('The widget')
 
     def test_resize(self):
         self.widget.resize(100, 150)
-        self.assertEqual(self.widget.size, (100, 150),
-                         'wrong size after resize')
+        self.assertEqual(self.widget.size, (100, 150), 'wrong size after resize')
 
     def test_default_size(self):
-        self.assertEqual(self.widget.size, (50, 50),
-                         'incorrect default size')
+        self.assertEqual(self.widget.size, (50, 50), 'incorrect default size')
 
+    def test_name(self):
+        self.assertEqual(self.widget.name, 'The widget', 'incorrect name')
+
+    #Срабатывает до тестов
+    def tearDown(self):
+        del self.widget
+
+
+class SkippingTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.widget = file4test.Widget('The widget')
+
+    @unittest.skip("demo skipping")
+    def test_nothing(self):
+        self.assertEqual('s', "e")
+
+    @unittest.skipIf(file4test.__version__ < 0.1, 'not supported version')
+    def test_var_q(self):
+        self.assertEqual(self.widget.q, ('foo', 'bar'), 'Incorrect q')
+
+    @unittest.skipUnless(file4test.__author__ == 'warior', 'Not you problem ')
+    def test_master(self):
+        self.assertEqual(file4test.__mastermind__,
+                         "https://docs.python.org/3/library/unittest.html",
+                         'incorrect master')
+
+
+def suite():
+    st = unittest.TestSuite()
+    st.addTest(WidgetTestCase('test_resize'))
+    st.addTest(WidgetTestCase('test_name'))
+    return st
